d46ea622613407bff933be7940682a4e3c0943db
Peter Hozák <peter.hozak@gmail.com>
2026-08-14 13:31:49 +0200

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
-// https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
+// https://rust-book.cs.brown.edu/ch05-02-example-structs.html
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    let maxx = 10_000;
-    let secret: u16 = rand::thread_rng().gen_range(1..=maxx);
-    println!("Guess a number 1-{maxx}: ");
+    let scale = 2;
+    let rect1 = Rect {
+        width: dbg!(30 * scale),
+        height: 50
+    };
 
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("should have been a line");
-        let guess: u16 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        match guess.cmp(&secret) {
-            Ordering::Equal => {
-                println!("Yup!");
-                break;
-            },
-            Ordering::Less => println!("Moar!"),
-            Ordering::Greater => println!("Not that much!"),
-        }
-    }
+    dbg!(&rect1);
+    println!("Area: {}", area(&rect1));
+}
+
+fn area(rect: &Rect) -> u32 {
+    rect.width * rect.height
 }
