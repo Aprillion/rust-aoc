506e75ce3fa6adbd818f2679784aec0ea693c402
Peter Hozák <peter.hozak@gmail.com>
2026-08-17 15:03:27 +0200

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-// https://rust-book.cs.brown.edu/ch05-02-example-structs.html
+// https://rust-book.cs.brown.edu/ch05-03-method-syntax.html
 #[derive(Debug)]
 struct Rect {
     width: u32,
     height: u32,
 }
-fn main() {
-    let scale = 2;
-    let rect1 = Rect {
-        width: dbg!(30 * scale),
-        height: 50
-    };
 
-    dbg!(&rect1);
-    println!("Area: {}", area(&rect1));
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rect) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
-fn area(rect: &Rect) -> u32 {
-    rect.width * rect.height
+fn main() {
+    let rect1 = Rect {
+        width: 30,
+        height: 50
+    };
+    dbg!(&rect1);
+    println!("Area: {}", rect1.area());
+
+    let mut rect2 = Rect {
+        width: 10,
+        height: 40,
+    };
+    rect2.width = 11;
+    let sq = Rect::square(60);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold sq? {}", rect1.can_hold(&sq));
 }
